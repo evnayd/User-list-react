e6f8ce3ab71c9b72a832b25b659f48f3ca2e4868
Evgeniya <evgeniya@ultimatemovers.net>
2022-08-31 15:15:33 +0200

    added api call for users

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import ContactCard from './components/ContactCard';
 
 function App() {
-  const contacts = [
-    { avatarUrl: 'https://via.placeholder.com/150',
-      name:'Jenny Han',
-      age:"30",
-      email:"mail@mail.com"},
-    { avatarUrl: 'https://via.placeholder.com/150',
-      name:'Bob Bob',
-      age:"39",
-      email:"mail39bob@mail.com"},
-    { avatarUrl: 'https://via.placeholder.com/150',
-      name:'Mila Mila',
-      age:"29",
-      email:"mila1@mail.com"}
-  ]
+
+  const [results,  setResults] = useState([]);
+
+  useEffect(() => { fetch('https://randomuser.me/api/?results=100')
+  .then(response => response.json())
+  .then(data =>  {
+    setResults(data.results)
+  });
+}, [])
+ 
+
   return (
     <div>
-      {contacts.map((contact, index) => {
+      {results.map((result, index) => {
         return (
         <ContactCard
         key = {index}
-        avatarUrl = {contact.avatarUrl}
-        name = {contact.name}
-        age = {contact.age}
-        email = {contact.email}
+        avatarUrl = {result.picture.large}
+        name = {result.name.first}
+        age = {result.dob.age}
+        email = {result.email}
         />
         )
       })}
